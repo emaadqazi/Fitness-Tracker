961f4ca4cbb43e88992c0274f56b9c8a02fb5345
Emaad Qazi <qaziemaad@gmail.com>
2025-08-03 16:51:19 -0400

    Fixing deployment issues for Railway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Main function where I will start app from
-
+import os
 from Website import create_app
 
 print("Creating app...")
@@ -7,4 +7,5 @@ app = create_app()
 
 if __name__ == '__main__': #If conditional makes sure we do not run it on accident 
     print("App is running..")
-    app.run(debug=True)  #Starts flask server
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
